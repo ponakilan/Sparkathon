47d28b300fd24289d81b13eed40ecad942016d30
ponakilan <ponakilan25@gmail.com>
2024-08-11 17:28:14 +0530

    Initial

--- src/forecast.py
+++ src/forecast.py
@@ -4,7 +4,7 @@ from PIL import Image
 def forecast() -> np.ndarray:
     return np.random.rand((15))
 
-def explain():
+def explain() -> tuple[Image.Image, Image.Image, str]:
     imarray = np.random.rand(224, 512, 3) * 255
     image = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
     return image, image, "Hello!!"
